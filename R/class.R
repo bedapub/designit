138fc02b624926e6f599f6955b16620e1d884841
Balazs Banfai <balazs.banfai@roche.com>
2020-05-29 16:38:07 +0200

    Add some notes from the meeting.

--- R/class.R
+++ R/class.R
@@ -3,6 +3,8 @@ Design <- R6::R6Class("Design",
     samples = NULL,
     batches = NULL,
     seed = NULL,
+    trace = NULL,
+    criterion = NULL,
 
     new_batch_container = function(
       # arguments
@@ -21,11 +23,27 @@ Design <- R6::R6Class("Design",
     distribute_samples = function(seed = NULL) {
       self$seed <- seed
       private$random_seed <- .Random.seed
+      private$validate_samples(self$samples)
       randomize(self$batches, self$samples)
     }
   ),
 
   private = list(
-    random_seed = NULL
+    random_seed = NULL,
+
+    validate_samples = function(x) {
+      # stopifnot(is_good(x))  # assertthat::assert_that()
+    }
+  )
+)
+
+
+Batch <- R6::R6Class("Batch",
+  public = list(
+
+  ),
+
+  private = list(
+
   )
 )
