cb4edc91b2b895b3c261c3297cf3f44b20dd9596
Iakov Davydov <iakov.davydov@roche.com>
2020-10-28 10:37:43 +0100

    move assignment to a separate file

---
+++ R/assignment.R
@@ -0,0 +1,28 @@
+#' Dummy assignment function which distributes samples randomly.
+#'
+#' @export
+#' @param samples data.frame with samples.
+#' @param batch_container Instance of BatchContainer class
+assign_random <- function(batch_container, samples = NULL) {
+  if (is.null(samples)) {
+    assertthat::assert_that(batch_container$has_samples(),
+      msg = "batch-container is empty and no samples provided"
+    )
+  } else {
+    batch_container$samples_df <- samples
+  }
+
+  n_samples <- nrow(batch_container$samples_df)
+  n_available <- batch_container$n_available
+
+  assertthat::assert_that(n_available >= n_samples)
+
+  expanded_ids <- c(
+    1:n_samples,
+    rep(NA_integer_, n_available - n_samples)
+  )
+
+  batch_container$assignment_vec <- sample(expanded_ids)
+
+  invisible(batch_container)
+}
--- R/class.R
+++ R/class.R
@@ -1,32 +1,3 @@
-#' Dummy assignment function which distributes samples randomly.
-#'
-#' @export
-#' @param samples data.frame with samples.
-#' @param batch_container Instance of BatchContainer class
-assign_random <- function(batch_container, samples = NULL) {
-  if (is.null(samples)) {
-    assertthat::assert_that(batch_container$has_samples(),
-      msg = "batch-container is empty and no samples provided"
-    )
-  } else {
-    batch_container$samples_df <- samples
-  }
-
-  n_samples <- nrow(batch_container$samples_df)
-  n_available <- batch_container$n_available
-
-  assertthat::assert_that(n_available >= n_samples)
-
-  expanded_ids <- c(
-    1:n_samples,
-    rep(NA_integer_, n_available - n_samples)
-  )
-
-  batch_container$assignment_vec <- sample(expanded_ids)
-
-  invisible(batch_container)
-}
-
 #' Validates sample data.frame.
 validate_samples <- function(samples) {
   assertthat::assert_that(is.data.frame(samples),
