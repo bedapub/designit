77fa4292309c9635d89e1295741c3cf004ceb818
Siebourg-Polster <siebourj@emea.roche.com>
2021-02-15 18:33:51 +0100

    add dataset documentation

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Unbalanced treatment and time sample list
+#'
+#' A sample list with 4 columns SampleName, Well, Time and Treatment
+#' Not all treatments are avaliable at all time points.
+#' All samples are placed on the same plate.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name multi_trt_day_samples
+#' @usage data(multi_trt_day_samples)
+#' @format An object of class \code{"tibble"}
+#'
+#' @author siebourj
+"multi_trt_day_samples"
+
