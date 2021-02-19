9c94228ff984ac7df06c98b0bad8b40033107057
Balazs Banfai <balazs.banfai@roche.com>
2021-02-19 14:51:43 +0100

    Update documentation and vignette

--- R/data.R
+++ R/data.R
@@ -1,7 +1,12 @@
 #' Mice sorted into cages
 #'
 #' Original arrival data of 40 mice, their treatment assignments, and
-#' their assignments into cages.
+#' their assignments into cages. Variables `Box`-`ArrivalWeight` are
+#' based on the arrival data of the animals. `Group` variable is the
+#' assigned treatment group, and `CageNr`, `CageCol`, and `CageRow`
+#' variables are the generated cage layout.
+#' Variables marked with an asterisk are important for the cage layout
+#' generation.
 #'
 #' @name cages
 #' @author Guido Steiner
@@ -12,19 +17,18 @@
 #' \describe{
 #' \item{Box}{Litter box}
 #' \item{BoxSection}{Section of litter box}
-#' \item{Litter}{Box&BoxSection}
-#' \item{Sex}{Female (F) or Male (M)}
+#' \item{*Litter}{`Box` & `BoxSection`}
+#' \item{*Sex}{Female (F) or Male (M)}
 #' \item{Earmark}{Side (_L_ left, _R_ right, or _B_ both),
 #'   and quantity (1-3, unspecified if only 1) of earmarks}
-#' \item{AnimalID}{}
-#' \item{BirthDate}{Date of birth (date)}
-#' \item{Genotype}{Genotype (MUT - mutant, WT - wild type)}
-#' \item{Comment}{Comments about the animal}
-#' \item{Gen}{SNPs of the animal}
-#' \item{ArrivalWeight}{Weight of the animal at arrival (g)}
-#' \item{Group}{Blinded treatment group (1 or 2)}
-#' \item{CageNr}{Cage number}
-#' \item{CageCol}{Column of cage rack}
-#' \item{CageRow}{Row of cage rack}
+#' \item{*AnimalID}{`Sex` - `Litter` - `Earmark`}
+#' \item{*BirthDate}{Date of birth (date)}
+#' \item{*Genotype}{Genotype (MUT - mutant, WT - wild type)}
+#' \item{Comment}{Comments about the animal (two of them are deformed)}
+#' \item{*ArrivalWeight}{Weight of the animal at arrival (g)}
+#' \item{*Group}{_(assigned)_ Blinded treatment group (1 or 2)}
+#' \item{CageNr}{_(generated)_ Cage number}
+#' \item{CageCol}{_(generated)_ Column of cage rack}
+#' \item{CageRow}{_(generated)_ Row of cage rack}
 #' }
 "cages"
