e9a6d6ddf1a65f212c8108b4ff8e1d3415604793
Balazs Banfai <balazs.banfai@roche.com>
2021-05-31 16:01:47 +0000

    Revert "Merge branch 'invivo_permute_with_constraints' into 'master'"
    This reverts merge request !19

--- R/assignment.R
+++ R/assignment.R
@@ -6,9 +6,25 @@
 #'
 #' @return Returns `BatchContainer`, invisibly.
 assign_random <- function(batch_container, samples = NULL) {
-  batch_container <- assign_in_order(batch_container, samples)
+  if (is.null(samples)) {
+    assertthat::assert_that(batch_container$has_samples,
+      msg = "batch-container is empty and no samples provided"
+    )
+  } else {
+    batch_container$samples_df <- samples
+  }
 
-  batch_container$assignment_vec <- sample(batch_container$assignment_vec)
+  n_samples <- nrow(batch_container$samples_df)
+  n_available <- batch_container$n_available
+
+  assertthat::assert_that(n_available >= n_samples)
+
+  expanded_ids <- c(
+    1:n_samples,
+    rep(NA_integer_, n_available - n_samples)
+  )
+
+  batch_container$assignment_vec <- sample(expanded_ids)
 
   invisible(batch_container)
 }
