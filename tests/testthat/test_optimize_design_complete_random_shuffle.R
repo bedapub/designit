6eb937ed3e5d34c45c12801985d03e48ccf5004b
Iakov Davydov <iakov.davydov@roche.com>
2021-10-19 17:13:38 +0200

    tests for optimize design: simple shuffle & complete random shuffle

---
+++ tests/testthat/test_optimize_design_complete_random_shuffle.R
@@ -0,0 +1,26 @@
+bc <- BatchContainer$new(
+  dimensions = c(location = 100)
+)
+
+samples <- data.frame(
+  sampleId = seq_len(98)
+)
+
+assign_in_order(bc, samples)
+
+# decreasing score
+bc$scoring_f <- (function() {score <- 1; function(...) {score <<- score - 1}})()
+
+set.seed(42)
+
+start_state <- ifelse(is.na(bc$assignment), -1, bc$assignment)
+
+test_that("correct number of shuffles = 1", {
+  optimize_design(
+    bc,
+    max_iter = 1,
+    shuffle_proposal_func=complete_random_shuffling
+  )
+  cur_state <- ifelse(is.na(bc$assignment), -1, bc$assignment)
+  expect_lt(sum(start_state == cur_state), 50)
+})
