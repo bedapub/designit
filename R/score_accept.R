629d261dde1924bc03924ddee9957664d67b99ab
steineg3 <guido.steiner@roche.com>
2022-07-12 17:26:12 +0200

    Add mk_exponentially_weighted_acceptance_func()

--- R/score_accept.R
+++ R/score_accept.R
@@ -39,3 +39,33 @@ accept_leftmost_improvement <- function(current_score, best_score, tol=1e-6, ...
   }
   if (any(current_score < best_score)) TRUE else FALSE
 }
+
+
+#' Alternative acceptance function for multi-dimensional scores with exponentially downweighted score improvements from left to right
+#'
+#' @param kappa Coefficient that determines how quickly the weights for the individual score improvements drop when going from left to right
+#' (i.e. first to last score). Weight for the first score's delta is 1, then the original delta multiplied with kappa^(p-1) for the p'th score
+#' @param simulated_annealing Boolean; if TRUE, simulated annealing (SA) will be used to minimize the weighted improved score
+#' @param temp_function In case SA is used, a temperature function that returns the annealing temperature for a certain iteration number
+#'
+#' @return Boolean, TRUE if current score should be taken as the new optimal score, FALSE otherwise
+#'
+#' @export
+mk_exponentially_weighted_acceptance_func <- function(kappa=0.5, simulated_annealing=FALSE,
+                                                      temp_function = mk_simanneal_temp_func(T0 = 500, alpha = 0.8)) {
+
+  force(kappa)
+  force(temp_function)
+  assertthat::assert_that(kappa>0, kappa<1, msg="Exponential weighting parameter kappa has to be in the open inteval ]0,1[")
+  if (simulated_annealing) {
+    acc_func = mk_simanneal_acceptance_func(temp_function=temp_function) # If SA, set up acceptance function with annealing protocol
+  } else {
+    acc_func = function(current_score, ...){ current_score < 0 } # else just check for an overall improvement of weighted sum of score differences
+  }
+
+  function(current_score, best_score, iteration) {
+    weighted_imp = sum((current_score - best_score)*kappa^(seq_along(current_score)-1))
+
+    acc_func(current_score = weighted_imp, best_score=0.0, iteration) # Comparing the improvement against 0.0 is actually deciding on the delta
+  }
+}
