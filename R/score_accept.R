f8de11b690a017460270d70113526b71f3730005
steineg3 <guido.steiner@roche.com>
2022-07-12 14:39:27 +0200

    Tested functionality of accept_leftmost_improvement() and adjusted documentation.

--- R/score_accept.R
+++ R/score_accept.R
@@ -20,17 +20,19 @@ accept_strict_improvement <- function(current_score, best_score, ...) {
 }
 
 
-#' Alternative acceptance function for optimizer. Accept current score if no )
+#' Alternative acceptance function for multi-dimensional scores in which order (left to right, e.g. first to last) denotes relevance.
 #'
 #' @param current_score One- or multi-dimensional score from the current optimizing iteration (double or vector of doubles)
 #' @param best_score Best one- or multi-dimensional score found so far (double or vector of doubles)
-#' @param tol Tolerance value
+#' @param tol Tolerance value: When comparing score vectors from left to right, differences within +/- tol won't immediately
+#' shortcut the comparison at this point, allowing improvement in a less important score to exhibit some influence
 #' @param ... Ignored arguments that may be used by alternative acceptance functions
 #'
 #' @return Boolean, TRUE if current score should be taken as the new optimal score, FALSE otherwise
 #'
-#' @keywords internal
+#' @export
 accept_leftmost_improvement <- function(current_score, best_score, tol=1e-6, ...) {
+
   for (i in seq_along(current_score)) {
     if (current_score[i] > best_score[i] + tol) return(FALSE)
     if (current_score[i] < best_score[i] - tol) return(TRUE)
