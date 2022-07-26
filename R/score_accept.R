d4f8295723fbfaccea185c0d40f8ef8209588710
steineg3 <guido.steiner@roche.com>
2022-07-26 16:51:12 +0200

    Renamed tolerance parameter: tol -> tolerance

--- R/score_accept.R
+++ R/score_accept.R
@@ -18,19 +18,19 @@ accept_strict_improvement <- function(current_score, best_score, ...) {
 #'
 #' @param current_score One- or multi-dimensional score from the current optimizing iteration (double or vector of doubles)
 #' @param best_score Best one- or multi-dimensional score found so far (double or vector of doubles)
-#' @param tol Tolerance value: When comparing score vectors from left to right, differences within +/- tol won't immediately
+#' @param tolerance Tolerance value: When comparing score vectors from left to right, differences within +/- tol won't immediately
 #' shortcut the comparison at this point, allowing improvement in a less important score to exhibit some influence
 #' @param ... Ignored arguments that may be used by alternative acceptance functions
 #'
 #' @return Boolean, TRUE if current score should be taken as the new optimal score, FALSE otherwise
 #'
 #' @export
-accept_leftmost_improvement <- function(current_score, best_score, ..., tol = 0.0) {
+accept_leftmost_improvement <- function(current_score, best_score, ..., tolerance = 0.0) {
   for (i in seq_along(current_score)) {
-    if (current_score[i] > best_score[i] + tol) {
+    if (current_score[i] > best_score[i] + tolerance) {
       return(FALSE)
     }
-    if (current_score[i] < best_score[i] - tol) {
+    if (current_score[i] < best_score[i] - tolerance) {
       return(TRUE)
     }
   }
