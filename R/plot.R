c70cb8829e53feb9124cc03d67bc8b3c424096b5
Balazs Banfai <balazs.banfai@roche.com>
2022-09-05 23:13:56 +0200

    Add dontrun for plate plot pattern

--- R/plot.R
+++ R/plot.R
@@ -85,13 +85,13 @@ plot_design <- function(.tbl, ..., .color, .alpha = NULL) {
 #'
 #' plot_plate(bc$get_samples(),
 #'   plate = plate, column = column, row = row,
-#'   .color = Treatment, .alpha = Timepoint
-#' )
+#'   .color = Treatment, .alpha = Timepoint)
 #'
-#' plot_plate(bc$get_samples(),
-#'   plate = plate, column = column, row = row,
-#'   .color = Treatment, .pattern = Timepoint
-#' )
+#' \dontrun{
+#'   plot_plate(bc$get_samples(),
+#'     plate = plate, column = column, row = row,
+#'     .color = Treatment, .pattern = Timepoint)
+#' }
 plot_plate <- function(.tbl, plate = plate, row = row, column = column,
                        .color, .alpha = NULL, .pattern = NULL,
                        title = paste("Layout by", rlang::as_name(rlang::enquo(plate))),
