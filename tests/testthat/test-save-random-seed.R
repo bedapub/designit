4bcd6743fde428785d8025bebbbb352f4376b3bd
Iakov Davydov <671660+idavydov@users.noreply.github.com>
2024-03-15 11:40:24 +0100

    test that random seed and rng kind are set

---
+++ tests/testthat/test-save-random-seed.R
@@ -0,0 +1,46 @@
+bc <- BatchContainer$new(
+  dimensions = c(location = 20)
+) |>
+  assign_in_order(
+    data.frame(
+      sampleId = seq_len(20)
+    )
+  )
+
+scoring_f <- function(...) rnorm(1)
+
+test_that("random seed is saved when no seed is set", {
+  # unset random seed
+  rm(.Random.seed, envir = .GlobalEnv)
+  bc_opt <- optimize_design(
+    bc,
+    scoring = scoring_f,
+    max_iter = 3
+  )
+  expect_s3_class(bc_opt$trace, "data.frame")
+  expect_equal(nrow(bc_opt$trace), 1)
+  expect_type(bc_opt$trace$seed, "list")
+  expect_type(bc_opt$trace$seed[[1]], "integer")
+  expect_gt(length(bc_opt$trace$seed[[1]]), 0)
+  expect_type(bc_opt$trace$rng_kind, "list")
+  expect_type(bc_opt$trace$rng_kind[[1]], "character")
+  expect_length(bc_opt$trace$rng_kind[[1]], 3)
+})
+
+
+test_that("random seed is saved when it was set", {
+  set.seed(6)
+  bc_opt <- optimize_design(
+    bc,
+    scoring = scoring_f,
+    max_iter = 3
+  )
+  expect_s3_class(bc_opt$trace, "data.frame")
+  expect_equal(nrow(bc_opt$trace), 1)
+  expect_type(bc_opt$trace$seed, "list")
+  expect_type(bc_opt$trace$seed[[1]], "integer")
+  expect_gt(length(bc_opt$trace$seed[[1]]), 0)
+  expect_type(bc_opt$trace$rng_kind, "list")
+  expect_type(bc_opt$trace$rng_kind[[1]], "character")
+  expect_length(bc_opt$trace$rng_kind[[1]], 3)
+})
